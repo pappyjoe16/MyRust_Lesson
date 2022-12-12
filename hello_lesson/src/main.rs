91db270a4c3fb4b2be59cd7d58a2d4d06a9ce847
Akinboyede, Joseph <jakinboy@volvocars.com>
2022-12-12 03:47:16 +0100

    Update wish

--- hello_lesson/src/main.rs
+++ hello_lesson/src/main.rs
@@ -1,5 +1,5 @@
 fn main(){
 	println!("Welcome to Rust Programming Joseph");
 	println!("I know you will do great leaning this programming language!");
-
+    println!("Wish you best Joseph in your 7learning adventure");
 }
