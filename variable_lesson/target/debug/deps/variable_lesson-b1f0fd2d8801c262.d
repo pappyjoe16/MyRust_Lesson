2089637850b517283853cfbb708aa79de554038a
Akinboyede Joseph <josephakinboyede16@gmail.com>
2023-04-15 04:18:40 +0200

    update variable target

--- variable_lesson/target/debug/deps/variable_lesson-b1f0fd2d8801c262.d
+++
@@ -1,5 +0,0 @@
-C:\Users\XPS\OneDrive\Documents\My_lessons\MyRust_Lesson\variable_lesson\target\debug\deps\variable_lesson-b1f0fd2d8801c262.rmeta: src\main.rs
-
-C:\Users\XPS\OneDrive\Documents\My_lessons\MyRust_Lesson\variable_lesson\target\debug\deps\variable_lesson-b1f0fd2d8801c262.d: src\main.rs
-
-src\main.rs:
