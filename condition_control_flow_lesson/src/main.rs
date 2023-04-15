aaab4f902c133f7540231dbd842089a2315f8fbc
Akinboyede Joseph <josephakinboyede16@gmail.com>
2023-04-15 03:43:15 +0200

    If statment, else if statment, else statment and funtion of .trim at the end of enter strings is to remove the special charater \n (new line)

--- condition_control_flow_lesson/src/main.rs
+++ condition_control_flow_lesson/src/main.rs
@@ -1,9 +1,18 @@
+use std::io;
+
 fn main() {
-    // to check for condition
-    // the operator we have in rust &&, ||, !.... out of three the order on value is !, && and ||
-    let cond1 = "Hello";
-    let cond2 = "Hello_Hello";
-    let cond3 = cond1 < cond2; 
+    let mut input = String::new();
+    println!("Enter your favorite food");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Expected type of food");
     
-    println!("{}", cond3);
+    if input.trim() == "Pizza"{
+        println!("I like Pizza too!");
+    }else if input.trim() == "Bread"{
+        println!("{}, That sounds boring!", input.trim());
+    } else {
+        println!("{}, Not my favorite!", input.trim());
+    }
+
 }
