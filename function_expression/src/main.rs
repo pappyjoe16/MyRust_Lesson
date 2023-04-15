f6c8cdddeca3cf7bba99ef3d0bd6a5708a26c17c
Akinboyede Joseph <josephakinboyede16@gmail.com>
2023-04-15 04:53:01 +0200

    learning different between statement and expression. how to return value from function and how to have both statment and expression from a function. how to use "return" keyword to return a value with or without ';'

--- function_expression/src/main.rs
+++ function_expression/src/main.rs
@@ -15,17 +15,21 @@ fn main() {
         .expect("Expected integer value");
     let num2: i64 = input2.trim().parse().unwrap(); // the .trim is to remove the invisible special character (new line)
 
-    add_numbers(num1, num2);
-    subtract_numbers(num1, num2);
+    let add_result = add_numbers(num1, num2);  // this is a statment because it did not return any value it only assign value to add_result
+    let sub_result = subtract_numbers(num1, num2);
+    println!("The sum of the two number is: {}", add_result);
+    println!("The sub the two number is: {}", sub_result);
 
 }
 
-fn add_numbers(x: i64, y: i64){
-    let sum = x + y;
-    println!("The sum of the two number is: {}", sum)
+fn add_numbers(x: i64, y: i64) -> i64 { // the arrow here tell the data type of the return value 
+   // x + y //this is an expression because it return a value and that is why we dont have ';' after because it is an expression
+    // To use ';' we can use return keyword 
+    return x + y; // with ';' you must use return keyword or you use return keyword without ';'
 }
 
-fn subtract_numbers(x: i64, y: i64){
-    let sub = x - y;
-    println!("The sum of the two number is: {}", sub)
+fn subtract_numbers(x: i64, y: i64) -> i64{
+    //x - y //without the return keyword do not put ';'
+    let result = x - y; //we can also have statement like this then expression must follow if we want to return any value
+    return result;
 }
